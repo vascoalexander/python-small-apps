45bf592a83660aa0c0867d760b6376c446b1ed61
vascoalexander <vascoalexander@gmail.com>
2024-08-18 11:56:30 +0200

    ui adjustments

--- image-viewer/app.py
+++ image-viewer/app.py
@@ -1,5 +1,7 @@
 # image-viewer
 import tkinter as tk
+from tkinter import ttk
+import ttkbootstrap as ttk
 from tkinter import filedialog
 from pathlib import Path
 import os
@@ -10,16 +12,25 @@ def load_images():
     """Load a list of images in the given path that have an extension defined
     in the image_ext List"""
     window.images = []
-    path = os.path.abspath(filedialog.askdirectory(initialdir=Path.home()))
-    for file in os.listdir(path):
-        if file.lower().endswith(image_ext):
-            image_path = os.path.join(path,file)
-            image = Image.open(image_path)
-            window.images.append(image)
-    if window.images:
-        display_image(0)
+    directory = filedialog.askdirectory(initialdir=Path.home())
+    # Checking if the dialog is canceld
+    if directory:
+
+        path = os.path.abspath(directory)
+        for file in os.listdir(path):
+            if file.lower().endswith(image_ext):
+                image_path = os.path.join(path,file)
+                image = Image.open(image_path)
+                window.images.append(image)
+
+        if window.images:
+            display_image(0)
+        else:
+            messagebox.showinfo(
+                title="Image Viewer", 
+                message=f"No displayable images in the folder!")          
     else:
-        messagebox.showinfo(title="Image Viewer", message=f"No displayable images in the folder!")
+        return
 
 def display_image(index):
     """Create a Label to display an image from a List of Images with the given
@@ -83,18 +94,46 @@ image_ext = (".jpeg", ".jpg", ".png", ".gif", ".bmp", ".webp")
 image_next = tk.PhotoImage(file='./image-viewer/icons/next.png')
 image_prev = tk.PhotoImage(file='./image-viewer/icons/prev.png')
 
+# widgets
+frame_bottom = tk.Frame(window, bg="#2b2b2b")
+
 button_open = tk.Button(
-    window, 
+    frame_bottom, 
     text="Open Folder", 
     font=("Arial", 14),
     command=load_images, 
     bg="#474747", 
-    fg="white")
-button_next = tk.Button(window, image=image_next, command=next_image, bg="#474747")
-button_prev = tk.Button(window, image=image_prev, command=prev_image, bg="#474747")
+    fg="white",
+    borderwidth=0
+)
 
+button_next = tk.Button(
+    window, 
+    image=image_next, 
+    command=next_image, 
+    bg="#2b2b2b", 
+    borderwidth=0,
+    highlightthickness=0,
+    activebackground="#2b2b2b",
+    takefocus=False
+)
+
+button_prev = tk.Button(
+    window, 
+    image=image_prev, 
+    command=prev_image, 
+    bg="#2b2b2b", 
+    borderwidth=0,
+    highlightthickness=0,
+    activebackground="#2b2b2b",
+    takefocus=False
+)
+
+# layout
 button_prev.pack(side="left")
-button_open.pack(side="bottom", padx=5,pady=5)
 button_next.pack(side="right")
+frame_bottom.pack(side="bottom", anchor="center")
+button_open.pack(side="bottom", padx=5,pady=5)
 
+# run
 window.mainloop()
